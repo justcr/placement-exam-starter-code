b710bb7ad429f47342d82de05fae070826b82ad5
Carlos Rodriguez <barlos0@Carloss-MacBook-Pro.local>
2021-10-05 19:35:38 -0700

    updated

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,83 @@
 
-//counter
-let gbCounter = 0;
-
-let qtygb = localStorage.getItem("qty-gb");
-
-
-let ccCounter = 0; 
-
-let qtycc = localStorage.getItem("qty-cc");
-
-
-let sugarCounter = 0;
-
-let qtysugar = localStorage.getItem("qty-sugar");
-
-
+let yourName = Carlos Rodriguez
+document.getElementById('credit').textContent = `Created by ${yourName}`
 
 //tbrow
-let gbTbRow = document.querySelector("qty-gb");
+let gbTbRow = document.querySelector("#qty-gb");
+let gbCounter = 0;
 
-let ccTbRow = document.querySelector("qty-cc");
+let gbCounter = localStorage.getItem("qty-gb");
+    if (gbCounter) {
+        gbTbRow.textContent = gbCounter;
+    };
 
-let sugarTbRow = document.querySelector("qty-sugar");
+   
+let ccTbRow = document.querySelector("#qty-cc");
+let ccCounter = localStorage.getItem("qty-cc");
+    if (ccCounter) {
+        ccTbRow.textContent = ccCounter;
+    };
 
-let totalTbRow = document.querySelector("qty-total");
+
+
+let sugarTbRow = document.querySelector("#qty-sugar");
+let sugarCounter = 0;
+
+let sugarCounter = localStorage.getItem("qty-sugar");
+    if (sugarCounter) {
+        sugarTbRow.textContent = sugarCounter;
+    };
+
+
+let totalTbRow = document.querySelector("#qty-total");
 
 //btns
-document.getElementById("add-gb").addEventListener("click",function(){
-    //console.log(gbTbRow);
-   gbCounter = gbCounter + 1;
-   gbTbRow.textContent = gbCounter;
-
-   localStorage.setItem("qty-gb", gbCounter);
+document.getElementById("add-gb").addEventListener("click",function() {
+    let gbCounter = parseInt(gbTbRow.textContent);
+    gbCounter = gbCounter + 1;
+    gbTbRow.textContent = (gbCounter);
+    localStorage.setItem("qty-gb", gbCounter);
+    console.log('Ginger bread + button was clicked!')
+    
 });
 
-document.getElementById("minus-gb").addEventListener("click",function(){
-    //console.log(gbTbRow);
-   gbCounter = gbCounter - 1;
-   gbTbRow.textContent = gbCounter;
-
-   localStorage.setItem("qty-gb", ccCounter);
+document.getElementById("minus-gb").addEventListener("click",function() {
+    let gbCounter = parseInt(gbTbRow.textContent);
+    gbCounter = gbCounter - 1;
+    gbTbRow.textContent = (gbCounter);
+    localStorage.setItem("qty-gb", ccCounter);
 });
 
-document.getElementById("add-cc").addEventListener("click",function(){
-    //console.log(ccTbRow);
-   ccCounter = ccCounter + 1;
-   ccTbRow.textContent = ccCounter;
-
-   localStorage.setItem("qty-cc", ccCounter);
+document.getElementById("add-cc").addEventListener("click",function() {
+    let ccCounter = parseInt(ccTbRow.textContent);
+    ccCounter = ccCounter + 1;
+    ccTbRow.textContent = (ccCounter);
+    localStorage.setItem("qty-cc", ccCounter);
 });
 
-document.getElementById("minus-cc").addEventListener("click",function(){
-    //console.log(ccTbRow);
-   ccCounter = ccCounter - 1;
-   ccTbRow.textContent = ccCounter;
-
-   localStorage.setItem("qty-cc", ccCounter);
+document.getElementById("minus-cc").addEventListener("click",function() {
+    let ccCounter = parseInt(ccTbRow.textContent);
+    ccCounter = ccCounter - 1;
+    ccTbRow.textContent = (ccCounter);
+    localStorage.setItem("qty-cc", ccCounter);
 });
 
-document.getElementById("add-sugar").addEventListener("click",function(){
-    //console.log(sugarTbRow);
+document.getElementById("add-sugar").addEventListener("click",function() {
+    let sugarCounter = parseInt(sugarTbRow.textContent);
     sugarCounter = sugarCounter + 1;
-    sugarTbRow.textContent = sugarCounter;
-
-   localStorage.setItem("qty-sugar", sugarCounter);
+    sugarTbRow.textContent = (sugarCounter);
+    localStorage.setItem("qty-sugar", sugarCounter);
 });
 
-document.getElementById("minus-sugar").addEventListener("click",function(){
-    //console.log(sugarTbRow);
+document.getElementById("minus-sugar").addEventListener("click",function() {
+    let sugarCounter = parseInt(sugarTbRow.textContent);
     sugarCounter = sugarCounter - 1;
-    sugarTbRow.textContent = sugarCounter;
-
-   localStorage.setItem("qty-sugar", sugarCounter);
+    sugarTbRow.textContent = (sugarCounter);
+    localStorage.setItem("qty-sugar", sugarCounter);
 });
 
 //total votes
 totalTbRow.textContent = gbCounter + ccCounter + sugarCounter;
 
-document.getElementById("credit").textContent = `Created by ${Carlos Rodriguez}`
+
+
